01ba0777d3e03f00b6e6c59e0987849b9193c486
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-14 23:01:45 +0700

    Initial commit

---
+++ lib/App/GetoptPanjangUtils.pm
@@ -0,0 +1,23 @@
+package App::GetoptPanjangUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Getopt::Panjang
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Getopt::Panjang>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Getopt::Panjang>
+
+=cut
